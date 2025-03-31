99119733c2df0bafef3e066adadcd668588b4f0a
KalaP. <85206184+Leah191@users.noreply.github.com>
2025-03-31 11:42:53 -0400

    Update UploadImage.jsx

--- src/pages/dashboard/admin/addProduct/UploadImage.jsx
+++ src/pages/dashboard/admin/addProduct/UploadImage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import axios from 'axios'
-import { getBaseUrl } from '../../../../utils/BaseURL';
+import { getBaseUrl } from '../../../../utils/baseURL';
 
 const UploadImage = ({ name, setImage }) => {
     const [loading, setLoading] = useState(false);
