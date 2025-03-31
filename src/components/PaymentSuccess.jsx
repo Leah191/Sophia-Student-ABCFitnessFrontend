a752d6f4eb21a6a13db5b5b3d0d0e5fcfc983b6a
KalaP. <85206184+Leah191@users.noreply.github.com>
2025-03-31 11:36:54 -0400

    Update PaymentSuccess.jsx

--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getBaseUrl } from '../utils/BaseURL';
+import { getBaseUrl } from '../utils/baseURL';
 import TimelineStep from './TimelineStep';
 
 const PaymentSuccess = () => {
