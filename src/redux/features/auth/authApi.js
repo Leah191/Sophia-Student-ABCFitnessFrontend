fa3f19077bdef7a709fa3e9882935f506f1d870c
KalaP. <85206184+Leah191@users.noreply.github.com>
2025-03-31 11:28:01 -0400

    Update authApi.js

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
-import { getBaseUrl } from "../../../utils/BaseURL"
+import { getBaseUrl } from "../../../utils/baseURL"
 
 const authApi = createApi({
   reducerPath: "authApi",
